efa5d6a1612f4fdd5909d8886f3830f8f02fb83c
Sebastian Puerta Hincapie <shincapie@cambridgeassociates.com>
2026-03-20 16:57:32 -0400

    ado.net implementation

---
+++ Garage/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace Garage.Models
+{
+    public class Employee
+    {
+        public int EmployeeId {  get; set; }
+        public string EmployeeName { get; set; }
+        public byte RoleId { get; set; }
+        public byte DepartmentId { get; set; }
+        public ushort ProjectId { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? TerminationDate { get; set; }
+    }
+}
---
+++ ado.net/Program.cs
@@ -0,0 +1,55 @@
+﻿using Garage.Models;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+using System.Data;
+
+static class Program
+{
+    /// <summary>
+    /// ADO.NET Review
+    /// </summary>
+    static void Main()
+    {
+        const string connectionString = "Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=MyCompany; Integrated Security=true";
+        //const string connectionString = "Server=(localdb)\\MSSQLLocalDB; Database=MyCompany; Trusted_Connection=True";
+
+        const string queryString = @$"SELECT * FROM dbo.Employees";
+
+        var employees = new List<Employee>();
+        using (SqlConnection connection = new(connectionString))
+        {
+            SqlCommand command = new(queryString, connection);
+
+            try
+            {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var employee = new Employee()
+                        {
+                            EmployeeId = reader.GetInt32(reader.GetOrdinal("EmployeeId")),
+                            EmployeeName = reader.GetString(reader.GetOrdinal("EmployeeName")),
+                            RoleId = reader.GetByte(reader.GetOrdinal("RoleId")),
+                            DepartmentId = reader.GetByte(reader.GetOrdinal("DepartmentId")),
+                            ProjectId = (ushort)reader.GetInt16(reader.GetOrdinal("ProjectId")),
+                            StartDate = reader.IsDBNull(reader.GetOrdinal("StartDate")) ? null : reader.GetDateTime(reader.GetOrdinal("StartDate")),
+                            TerminationDate = reader.IsDBNull(reader.GetOrdinal("TerminationDate")) ? null : reader.GetDateTime(reader.GetOrdinal("TerminationDate"))
+                        };
+                        employees.Add(employee);
+                    }
+                }
+                connection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // output
+        Console.Write(JsonConvert.SerializeObject(employees, Formatting.Indented));
+    }
+}
