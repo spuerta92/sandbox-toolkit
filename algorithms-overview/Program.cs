479a61b97c94a194e145107a4a6ddf31ff5ff42e
Sebastian Puerta Hincapie <shincapie@cambridgeassociates.com>
2026-03-26 10:46:24 -0400

    added data structures, algorithms overview and useful shell/git commands.

---
+++ algorithms-overview/Program.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+
+static class Program
+{
+    static List<int> HeapSort(List<int> list)
+    {
+        // time complexity
+
+        // algorithm
+
+        return list;
+    }
+
+    static void Main()
+    {
+        var numbers = new List<int>
+        {
+            482, 17, 903, 256, 771, 64, 390, 998, 145, 623,
+            712, 88, 534, 301, 947, 219, 670, 41, 856, 193,
+            764, 509, 12, 681, 432, 975, 278, 156, 820, 367,
+            594, 733, 25, 908, 461, 102, 687, 314, 849, 57,
+            926, 238, 775, 490, 163, 604, 351, 992, 76, 547,
+            280, 819, 134, 665, 403, 951, 210, 742, 519, 30,
+            884, 297, 658, 171, 930, 446, 599, 83, 768, 324,
+            915, 251, 682, 140, 537, 996, 389, 72, 844, 213,
+            701, 478, 19, 862, 335, 620, 157, 973, 286, 541,
+            90, 754, 418, 629, 204, 887, 363, 146, 958, 507 
+        };
+
+        // heap sort
+        var heap_sorted = HeapSort(numbers);
+        Console.WriteLine(JsonConvert.SerializeObject(heap_sorted));
+
+        // merge sort
+
+        // quick sort
+
+        // linear search
+
+        // binary search
+
+        // breadth-first search
+
+        // depth-first search
+
+        // hashing
+
+        // recursion
+
+        // trees
+
+        // graphs
+    }
+}
--- data-structures-overview/Program.cs
+++ data-structures-overview/Program.cs
@@ -1,2 +1,168 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.ObjectModel;
+using toolbox.Enums;
+using toolbox.Models;
+
+static class Program
+{
+    static void Main()
+    {
+        // array
+        var employeeArray = new Employee[]
+        {
+            new Employee
+            {
+                EmployeeId = 1,
+                EmployeeName = "Charlie"
+            },
+            new Employee
+            {
+                EmployeeId = 2,
+                EmployeeName = "Will"
+            },
+            new Employee
+            {
+                EmployeeId = 3,
+                EmployeeName = "Arthur"
+            }
+        };
+        Console.WriteLine("Array");
+        Console.WriteLine(JsonConvert.SerializeObject(employeeArray));
+        Console.WriteLine();
+
+        // list 
+        var employeeList = new List<Employee>
+        {
+            new Employee
+            {
+                EmployeeId = 1,
+                EmployeeName = "Charlie"
+            },
+            new Employee
+            {
+                EmployeeId = 2,
+                EmployeeName = "Will"
+            },
+            new Employee
+            {
+                EmployeeId = 3,
+                EmployeeName = "Arthur"
+            }
+        };
+        Console.WriteLine("List");
+        Console.WriteLine(JsonConvert.SerializeObject(employeeList));
+        Console.WriteLine();
+
+        // collection
+        var employeeCollection = new Collection<Employee>
+        {
+            new Employee
+            {
+                EmployeeId = 1,
+                EmployeeName = "Charlie"
+            },
+            new Employee
+            {
+                EmployeeId = 2,
+                EmployeeName = "Will"
+            },
+            new Employee
+            {
+                EmployeeId = 3,
+                EmployeeName = "Arthur"
+            }
+        };
+        Console.WriteLine("Collections");
+        Console.WriteLine(JsonConvert.SerializeObject(employeeCollection));
+        Console.WriteLine();
+
+        // (enumerable -> collection -> list)
+        IEnumerable<Employee> employeeEnumerable = employeeList;
+        IEnumerable<Employee> EmployeeEnumberable2 = employeeCollection;
+        Console.WriteLine("IEnumerable (List)");
+        Console.WriteLine(JsonConvert.SerializeObject(employeeEnumerable));
+        Console.WriteLine();
+        Console.WriteLine("IEnumerable (Collection)");
+        Console.WriteLine(JsonConvert.SerializeObject(EmployeeEnumberable2));
+        Console.WriteLine();
+
+        // hash table (non-generic)
+        var employeeHashTable = new Hashtable();
+        employeeHashTable["name"] = "Charlie";
+        employeeHashTable["id"] = 1;
+
+        // hash set
+        var employeeHashSet = new HashSet<Employee>()
+        {
+            new Employee
+            {
+                EmployeeId = 1,
+                EmployeeName = "Charlie"
+            },
+            new Employee
+            {
+                EmployeeId = 2,
+                EmployeeName = "Will"
+            },
+            new Employee
+            {
+                EmployeeId = 3,
+                EmployeeName = "Arthur"
+            }
+        };
+        Console.WriteLine("Hashset");
+        Console.WriteLine(JsonConvert.SerializeObject(employeeHashSet));
+        Console.WriteLine();
+
+        // dictionary (generic)
+        var employeeDictionary = new Dictionary<Guid, Employee>
+        {
+            { Guid.NewGuid(), new Employee { EmployeeId = 1, EmployeeName = "Charlie"} },
+            { Guid.NewGuid(), new Employee { EmployeeId = 2, EmployeeName = "Will"} },
+            { Guid.NewGuid(), new Employee { EmployeeId = 3, EmployeeName = "Arthur"} }
+        };
+        Console.WriteLine("Dictionary");
+        Console.WriteLine(JsonConvert.SerializeObject(employeeDictionary));
+        Console.WriteLine();
+
+        // key/value pair
+        var employeeKV = new KeyValuePair<Guid, Employee>(Guid.NewGuid(), new Employee { EmployeeId = 1, EmployeeName = "Charlie" });
+        Console.WriteLine("Key Value Pair");
+        Console.WriteLine(JsonConvert.SerializeObject(employeeKV));
+        Console.WriteLine();
+
+        // tuple
+        var employeeTuple = new Tuple<Guid, Employee, Departments>(Guid.NewGuid(), new Employee { EmployeeId = 1, EmployeeName = "Charlie" }, Departments.InvestmentResearch);
+        Console.WriteLine("Tuple");
+        Console.WriteLine(JsonConvert.SerializeObject(employeeTuple));
+        Console.WriteLine();
+
+        // stack (LIFO)
+        var employeeStack = new Stack<Employee>();
+        employeeStack.Push(new Employee { EmployeeId = 1, EmployeeName = "Charlie" });
+        employeeStack.Push(new Employee { EmployeeId = 2, EmployeeName = "Will" });
+        employeeStack.Push(new Employee { EmployeeId = 3, EmployeeName = "Arthur" });
+        Console.WriteLine("Stack");
+        Console.WriteLine(JsonConvert.SerializeObject(employeeStack));
+        Console.WriteLine();
+
+        // queue (FIFO)
+        var employeeQueue = new Queue<Employee>();
+        employeeQueue.Enqueue(new Employee { EmployeeId = 1, EmployeeName = "Charlie" });
+        employeeQueue.Enqueue(new Employee { EmployeeId = 2, EmployeeName = "Will" });
+        employeeQueue.Enqueue(new Employee { EmployeeId = 3, EmployeeName = "Arthur" });
+        Console.WriteLine("Queue");
+        Console.WriteLine(JsonConvert.SerializeObject(employeeQueue));
+        Console.WriteLine();
+
+        // linked list
+        var employeeLinkedList = new LinkedList<Employee>();
+        employeeLinkedList.AddLast(new Employee { EmployeeId = 1, EmployeeName = "Charlie" });
+        employeeLinkedList.AddLast(new Employee { EmployeeId = 2, EmployeeName = "Will" });
+        employeeLinkedList.AddLast(new Employee { EmployeeId = 3, EmployeeName = "Arthur" });
+        Console.WriteLine("Linked List");
+        Console.WriteLine(JsonConvert.SerializeObject(employeeLinkedList));
+        Console.WriteLine();
+    }
+}
